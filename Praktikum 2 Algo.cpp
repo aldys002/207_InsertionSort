4c94c0044f8391116dff585f53d3a99e7558b31a
aldys002 <igidiaaldys@gmail.com>
2024-03-14 15:18:55 +0700

    menambah titik koma pada return 0

--- Praktikum 2 Algo.cpp
+++ Praktikum 2 Algo.cpp
@@ -3,10 +3,31 @@
 
 #include <iostream>
 using namespace std;
+int n, arr[20];
+
+void input() {
+    while (true) {
+        cout << "Masukkan banyaknya elemen pada Array : ";
+        cin >> n;
+        if (n <= 20)
+            break;
+        else {
+            cout << "\nArray dapat mempunyai maksimal 20 elemen.\n";
+        }
+    }
+    cout << endl;
+    cout << "===============" << endl;
+    cout << "masukan Elemen Array" << endl;
+    cout << "===============" << endl;
+
+    for (int i = 0; i < n; i++) {
+        cout << "data ke - " << (i + 1) << ":";
+        cin >> arr[i];
+    }
+}
 
 void insertionSort() {
-    int i, j, n, temp;
-    int arr[20];
+    int i, j, temp;
 
     for (i = 1; i <= n - 1; i++) { //step 1
         temp = arr[i];  //step 2
@@ -23,10 +44,28 @@ void insertionSort() {
     }
 }
 
+void display() {
+    cout << endl;
+    cout << "===================================" << endl;
+    cout << "Element Array yang telah tersusun" << endl;
+    cout << "===================================" << endl;
+    for (int j = 0; j < n; j++) {
+        cout << arr[j] << endl; //Output setiap elemen array pada garis baru
 
-int main()
-{
-    std::cout << "Hello World!\n";
+    }
+    cout << "jumlah pass = " << n - 1 << endl; //menampilkan total angka dari elemen yang benar
+    cout << endl;
 }
+
+    int main()
+    {
+        input();
+        insertionSort();
+        display();
+        system("Pause");
+        return 0;
+    }
+    
+
 
 
