eb2464efce9dc4457db2e81b044e72b93f7031fd
aldys002 <igidiaaldys@gmail.com>
2024-03-14 14:47:19 +0700

    add step 3

--- Praktikum 2 Algo.cpp
+++ Praktikum 2 Algo.cpp
@@ -9,8 +9,15 @@ void insertionSort() {
     int arr[20];
 
     for (i = 1; i <= n - 1; i++) { //step 1
-        temp = arr[1]; //step 2
+        temp = arr[i];  //step 2
 
+        j = i - 1; //step 3
+        
+        while (j >= 0 && arr[j] > temp) //step 4
+        {
+            arr[j] = arr[j + 1]; //step 4a
+            j = j - 1; //step 4b
+        }
     }
 }
 
